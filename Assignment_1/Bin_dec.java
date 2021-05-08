f53e4a8a895a0875bb2696d966882c9ebad35d79
Viresh2217 <83824549+Viresh2217@users.noreply.github.com>
2021-05-08 22:03:30 +0530

    Add files via upload

--- Assignment_1/Bin_dec.java
+++ Assignment_1/Bin_dec.java
@@ -1,34 +1,20 @@
+  
 import java.util.Scanner;
+public class Bin_dec {
+ public static void main(String[] args) 
+ {
+  Scanner sc = new Scanner(System.in);
+  long binaryNumber, decimalNumber = 0, j = 1, remainder;
+  System.out.print("Input a binary number: ");
+  binaryNumber = sc.nextLong();
 
-class Bin_dec{
-public static void main(String args[])
-{
-	int a,i,temp;  
-	i=0;
-	temp=0;
-      System.out.println("Enter Binary Number");
-	   Scanner pc=new Scanner(System.in); 
-	  a=pc.nextInt();
-	
-	 while(a!=0)
-	 {
-                  double power=Math.pow(2,i);
-		 temp=temp+(int)power*(a%10);
-		 a=a/10;
-		 i++;	
-            
-	 }
-	   System.out.println("Decimal Value is: "+temp); 
-
-}
-
-}
-/*while(a!=0){
-			
-			int k = a%10;
-			double power = Math.pow(2,i);
-			temp = temp + (k* (int)power);
-			a= a/10;
-			i++;
-		}
-*/
+  while (binaryNumber != 0) 
+  {
+   remainder = binaryNumber % 10;
+   decimalNumber = decimalNumber + remainder * j;
+   j = j * 2;
+   binaryNumber = binaryNumber / 10;
+  }
+  System.out.println("Decimal Number: " + decimalNumber);
+ }
+ }
