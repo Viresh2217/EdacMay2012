29935c1c7c927570fc0fba39e351b89915d41757
Viresh2217 <83824549+Viresh2217@users.noreply.github.com>
2021-05-24 10:10:19 +0530

    Add files via upload

---
+++ doubt/Movie_Review.java
@@ -0,0 +1,104 @@
+package Day_1;
+
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Movie_Review {
+	static int i=0;
+	public static void main(String[] args) throws InterruptedException, IOException {
+		
+      Review[] st=new Review[100];
+		
+		Scanner s=new Scanner(System.in);
+		 while(true)
+		 {
+			 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			 
+			
+
+			 System.out.println("1_Add movie review");
+	          System.out.println("2_Show movie review");
+	                             
+	          System.out.println("---------------------------------------------------------------------------------------") ;  
+	          System.out.println("Please enter your choice[1--2]");   
+	               int choice= s.nextInt();
+	                      
+	          switch (choice)
+	          {                   
+	                     case 1: 
+	                    	 
+        			         st[i]=new Review();
+        			         st[i].createReview();
+        			         System.out.println("Your review is Stored Sucessfully......!");
+        			         System.out.println("Thank....You....!");
+        		         	 System.out.println();
+        			         ++i;
+        			         break;
+	                     case 2:
+	                    	 System.out.println("Enter a movie name");
+	                    	 System.out.println();
+	                    	 
+	                    	 String mov=s.next();
+	                    	     
+	                              for(Review op:st)
+	                              {
+	                            	  op.show(mov);
+	                            	  
+	                              }
+	                              break;
+	                      default:
+	                    	  System.out.println("Please enter valid moview name");
+	              
+	          }
+		
+		 }
+	}
+
+}
+
+class Review{
+	private String Moviename;
+	private String director;
+	private double imdb;
+	private String review;
+	private String name;
+	
+	Scanner s=new Scanner(System.in);
+	
+	void getDetails()
+	{
+		System.out.println("Movie name:- "+Moviename);
+		System.out.println("Director name:- "+director);
+		System.out.println("IMDB rating:- "+imdb);
+		System.out.println("Openion about moview:- "+review);
+		System.out.println("This is review of:- "+name);
+		
+	}
+	
+	void createReview()
+	{
+		System.out.println("Enter a your name");
+		name=s.nextLine();
+		System.out.println("Enter a movie name");
+		Moviename=s.nextLine();
+		System.out.println("Enter a movie director name");
+		director=s.nextLine();
+		System.out.println("Enter a IMDB rating out of 10");
+		imdb=s.nextInt();
+		System.out.println("Enter your openion: ");
+		s.nextLine();
+		review=s.nextLine();
+	
+	}
+	void show(String name)
+	{
+		if(Moviename.equals(name))
+		{
+			getDetails();
+		}
+	
+	}
+	
+	
+}
